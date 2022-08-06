ba3fedb34ef551792feec07ffc165bceecbcfb91
vconcept <yungaeronaut@gmail.com>
2022-08-06 15:08:49 +0100

    files added

---
+++ my_printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "main.h"
+
+/**
+ * _printf - returns number of characters printed (excluding null byte)
+ *
+ * Return: Always 0
+ */
+
+ int _printf(const char *format, ...)
+{
+
